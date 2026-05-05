3a8f005087f30fc6fb83974a0d52b64babbcaa8c
johnsrei-png <johnsrei@gmail.com>
2026-05-05 10:32:56 -0700

    Create upabase-client.js

---
+++ netlify/functions/upabase-client.js
@@ -0,0 +1,10 @@
+netlify/
+  functions/
+    supabase-client.js          ← ADD THIS FILE
+    get-public-data.js          ← UPDATE with new version
+    update-salary.js            ← UPDATE with new version
+    advance-season.js
+    update-draft-pick.js
+    sync-league.js
+public/
+  index.html                    ← UPDATE with index-working.html
